58fbc69f6d91f77b3440ff7fc1b8a958d739d84a
Wojciech Pobocha <01168909@pw.edu.pl>
2023-04-05 15:26:01 +0200

    Initially created User and UserRepository to communicate with database.

--- src/main/java/com/app/TrainingWebApp/user/User.java
+++ src/main/java/com/app/TrainingWebApp/user/User.java
@@ -1,4 +1,59 @@
 package com.app.TrainingWebApp.user;
 
-public class User {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="_user")
+
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
