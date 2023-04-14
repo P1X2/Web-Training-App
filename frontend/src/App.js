5cd29d7a438a1e3e2350a9c7cf20f40da0ac6341
Wojciech Pobocha <01168909@pw.edu.pl>
2023-04-14 16:09:24 +0200

    Implemented saving jwt in local storage of the site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import sendRequest from "./util/ajax";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./homepage/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./login/Login";
 
 function App() {
-  console.log(sendRequest("rest/auth//get?username=wojtek398", "GET"));
-  return <Routes></Routes>;
+  // console.log(sendRequest("rest/auth//get?username=wojtek398", "GET"));
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
