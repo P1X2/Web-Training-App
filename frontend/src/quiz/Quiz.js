64a8de649c53e5b2d2a5160e2fabca383b840a35
Natalia <natalia@pop-os.localdomain>
2023-05-31 21:47:02 +0200

    creating plan after making quiz

--- frontend/src/quiz/Quiz.js
+++ frontend/src/quiz/Quiz.js
@@ -1,29 +1,31 @@
 import React, { useState } from "react";
 import { Container, Form, Button, Row } from "react-bootstrap";
+import sendRequest from "../util/ajax";
+import { useLocalState } from "../util/useLocalStorage";
 
 const Quiz = () => {
-  const [goal, setGoal] = useState('');
-  const [age, setAge] = useState('');
-  const [gender, setGender] = useState('');
-  const [currentWeight, setCurrentWeight] = useState('');
-  const [trainingFrequency, setTrainingFrequency] = useState('');
-  const [trainingDuration, setTrainingDuration] = useState('');
-  const [muscleGroup, setMuscleGroup] = useState('');
-  const [trainingIntensity, setTrainingIntensity] = useState('');
-  const [experienceLevel, setExperienceLevel] = useState('');
-    const [trained, setTrained] = useState('');
-    const [time, setTime] = useState('');
-    const [intensity, setIntensity] = useState('');
-    const [experienceScore, setExperienceScore] = useState("");
-
+  const [goal, setGoal] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [currentWeight, setCurrentWeight] = useState("");
+  const [trainingFrequency, setTrainingFrequency] = useState("");
+  const [trainingDuration, setTrainingDuration] = useState("");
+  const [muscleGroup, setMuscleGroup] = useState("");
+  const [trainingIntensity, setTrainingIntensity] = useState("");
+  const [experienceLevel, setExperienceLevel] = useState("");
+  const [trained, setTrained] = useState("");
+  const [time, setTime] = useState("");
+  const [intensity, setIntensity] = useState("");
+  const [experienceScore, setExperienceScore] = useState("");
+  const [jwt, setJwt] = useLocalState("", "jwt");
 
   const handleGoalChange = (e) => {
     setGoal(e.target.value);
   };
 
-    const handleTrainedChange = (e) => {
-      setTrained(e.target.value);
-    };
+  const handleTrainedChange = (e) => {
+    setTrained(e.target.value);
+  };
 
   const handleAgeChange = (e) => {
     setAge(e.target.value);
@@ -59,117 +61,120 @@ const Quiz = () => {
     // Determine experience level
     let experienceLevel = "";
 
-    let experiencePoints=0;
-    let experiencePointsMultiplayer=0;
-    let experienceScore=0;
+    let experiencePoints = 0;
+    let experiencePointsMultiplayer = 0;
+    let experienceScore = 0;
 
-    let intensity="";
+    let intensity = "";
     let time = 0;
     //wiek war
     if (age < 18) {
       alert("You can use this service only if you are older than 18");
       return;
-        }
-
-    if (currentWeight <=40) {
-      alert("Invalid weight - you need to weight at least 40kg to use our services");
-      return;
-        }
-
-    if (trainingDuration<=20 || trainingFrequency<=0) {
-      alert("Invalid input - check your answers");
-      return;
-        }
-    // wiek points
-    if (age >=18 && age <=30) {
-      experiencePoints+=15;
-            }
-    if (age >=31 && age <=50) {
-      experiencePoints+=20;
-            }
-    if (age >=51) {
-      experiencePoints+=10;
-            }
-    // plec=
-    if (gender==="male") {
-          experiencePointsMultiplayer+=1;
-                }
-    if (gender==="female") {
-          experiencePointsMultiplayer+=0.7;
-                }
-    // waga
-    if (currentWeight<60) {
-          experiencePoints+=5;
-                }
-    if (currentWeight>=60 && currentWeight<=80) {
-          experiencePoints+=10;
-                }
-    if (currentWeight>=81) {
-          experiencePoints+=20;
-                }
-    // trained
-    if (trained==="yes") {
-          experiencePointsMultiplayer+=0.5;
     }
 
-    if (trained==="no") {
-          experiencePointsMultiplayer+=0;
+    if (currentWeight <= 40) {
+      alert(
+        "Invalid weight - you need to weight at least 40kg to use our services"
+      );
+      return;
+    }
+
+    if (trainingDuration <= 20 || trainingFrequency <= 0) {
+      alert("Invalid input - check your answers");
+      return;
+    }
+    // wiek points
+    if (age >= 18 && age <= 30) {
+      experiencePoints += 15;
+    }
+    if (age >= 31 && age <= 50) {
+      experiencePoints += 20;
+    }
+    if (age >= 51) {
+      experiencePoints += 10;
+    }
+    // plec=
+    if (gender === "male") {
+      experiencePointsMultiplayer += 1;
+    }
+    if (gender === "female") {
+      experiencePointsMultiplayer += 0.7;
+    }
+    // waga
+    if (currentWeight < 60) {
+      experiencePoints += 5;
+    }
+    if (currentWeight >= 60 && currentWeight <= 80) {
+      experiencePoints += 10;
+    }
+    if (currentWeight >= 81) {
+      experiencePoints += 20;
+    }
+    // trained
+    if (trained === "yes") {
+      experiencePointsMultiplayer += 0.5;
+    }
+
+    if (trained === "no") {
+      experiencePointsMultiplayer += 0;
     }
 
     // ciezkie czy lekkie
-    if (trainingIntensity==="light") {
-          intensity="Medium";
-            experiencePointsMultiplayer+=0.0;
-          }
+    if (trainingIntensity === "light") {
+      intensity = "Medium";
+      experiencePointsMultiplayer += 0.0;
+    }
 
-    if (trainingIntensity==="heavy") {
-          intensity="High";
-            experiencePointsMultiplayer+=0.3;
-          }
-
+    if (trainingIntensity === "heavy") {
+      intensity = "High";
+      experiencePointsMultiplayer += 0.3;
+    }
 
     experienceScore = experiencePoints * experiencePointsMultiplayer;
 
-    if (experienceScore>=40) {
-        experienceLevel="Advanced";
-         }
+    if (experienceScore >= 40) {
+      experienceLevel = "advanced";
+    }
 
-    if (experienceScore<=40 && experienceScore>=25) {
-        experienceLevel="Intermediate";
-         }
+    if (experienceScore <= 40 && experienceScore >= 25) {
+      experienceLevel = "intermediate";
+    }
 
-    if (experienceScore<25) {
-        experienceLevel="Begginer";
-        }
-
-
-
-
+    if (experienceScore < 25) {
+      experienceLevel = "beginner";
+    }
 
     setExperienceScore(experienceScore);
     setTime(trainingDuration * trainingFrequency);
     setIntensity(intensity);
     setExperienceLevel(experienceLevel);
+
+    const requestBody = {
+      mainTarget: goal,
+      advancement: experienceLevel,
+      weight: currentWeight,
+      mainMuscleGroup: muscleGroup,
+    };
+
+    try {
+      console.log(requestBody);
+      sendRequest("rest/plan/create", "POST", jwt, requestBody);
+    } catch (error) {
+      console.error("Error occurred while creating plan", error);
+    }
   };
-
-
-
-
 
   return (
     <div className="container-fluid">
-        <div class="row">
-            <div class="col-sm">
-              <h1 class="text-center pt-3 display-2">Gym Quiz</h1>
-            </div>
-
+      <div class="row">
+        <div class="col-sm">
+          <h1 class="text-center pt-3 display-2">Gym Quiz</h1>
         </div>
-
-
+      </div>
 
       <div class="row">
-            <div class="col-2">
-            </div>
+        <div class="col-2"></div>
 
         <div class="col">
           <form onSubmit={handleSubmit}>
@@ -183,9 +188,9 @@ const Quiz = () => {
                 required
               >
                 <option value="">Select training goal</option>
-                <option value="weightLoss">Weight Loss</option>
-                <option value="muscleGain">Muscle Gain</option>
-                <option value="strengthBuilding">Strength Building</option>
+                <option value="stamina">Stamina</option>
+                <option value="growth">Growth</option>
+                <option value="strength">Strength</option>
               </select>
             </div>
             <p></p>
@@ -218,7 +223,9 @@ const Quiz = () => {
             </div>
             <p></p>
             <div className="form-group pb-2">
-              <label htmlFor="currentWeight">What is your current weight (in kg)?</label>
+              <label htmlFor="currentWeight">
+                What is your current weight (in kg)?
+              </label>
               <input
                 type="number"
                 className="form-control"
@@ -230,22 +237,26 @@ const Quiz = () => {
             </div>
             <p></p>
             <div className="form-group pb-2">
-              <label htmlFor="trained">Have you ever trained in the gym before?</label>
+              <label htmlFor="trained">
+                Have you ever trained in the gym before?
+              </label>
               <select
-                  className="form-control"
-                  id="trained"
-                  value={trained}
-                  onChange={handleTrainedChange}
-                  required
-                >
-                  <option value="">Select answer</option>
-                  <option value="yes">Yes</option>
-                  <option value="no">No</option>
-                </select>
+                className="form-control"
+                id="trained"
+                value={trained}
+                onChange={handleTrainedChange}
+                required
+              >
+                <option value="">Select answer</option>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+              </select>
             </div>
             <p></p>
             <div className="form-group pb-2">
-              <label htmlFor="trainingFrequency">How many training sessions per week do you want to have?</label>
+              <label htmlFor="trainingFrequency">
+                How many training sessions per week do you want to have?
+              </label>
               <input
                 type="number"
                 className="form-control"
@@ -257,7 +268,10 @@ const Quiz = () => {
             </div>
             <p></p>
             <div className="form-group pb-2">
-              <label htmlFor="trainingDuration">How long do you want your training sessions to last (in minutes)?</label>
+              <label htmlFor="trainingDuration">
+                How long do you want your training sessions to last (in
+                minutes)?
+              </label>
               <input
                 placeholder="Must be grater than 20min"
                 type="number"
@@ -270,7 +284,9 @@ const Quiz = () => {
             </div>
             <p></p>
             <div className="form-group pb-2">
-              <label htmlFor="muscleGroup">Which muscle group do you want to focus on the most?</label>
+              <label htmlFor="muscleGroup">
+                Which muscle group do you want to focus on the most?
+              </label>
               <select
                 className="form-control"
                 id="muscleGroup"
@@ -279,17 +295,19 @@ const Quiz = () => {
                 required
               >
                 <option value="">Select prio muscle group</option>
-                <option value="chest">Chest</option>
-                <option value="back">Back</option>
-                <option value="legs">Legs</option>
-                <option value="shoulders">Shoulders</option>
-                <option value="arms">Arms</option>
-                <option value="abs">Abs</option>
+                <option value="CHEST">Chest</option>
+                <option value="BACK">Back</option>
+                <option value="LEGS">Legs</option>
+                <option value="SHOULDER">Shoulders</option>
+                <option value="ARMS">Arms</option>
+                <option value="ABS">Abs</option>
               </select>
             </div>
             <p></p>
             <div className="form-group pb-2">
-              <label htmlFor="trainingIntensity">Do you want to have light or heavy training sessions?</label>
+              <label htmlFor="trainingIntensity">
+                Do you want to have light or heavy training sessions?
+              </label>
               <select
                 className="form-control"
                 id="trainingIntensity"
@@ -302,12 +320,15 @@ const Quiz = () => {
                 <option value="heavy">Heavy</option>
               </select>
             </div>
-              <div class="row text-center py-5">
-                <button type="submit"  className="btn btn-warning btn-lg btn-block">Submit your answers</button>
-              </div>
-
+            <div class="row text-center py-5">
+              <button
+                type="submit"
+                className="btn btn-warning btn-lg btn-block"
+              >
+                Submit your answers
+              </button>
+            </div>
           </form>
-
 
           {experienceLevel && (
             <div className="mt-3 fs-5">
@@ -316,29 +337,24 @@ const Quiz = () => {
               <p>Estimated experience level: {experienceLevel}</p>
               <p>Training intensity : {intensity}</p>
               <p>Time needed per week (in minutes) : {time}</p>
-
-              <p>Proposed training plan</p>
-
-
-
-
-
-              //proponowany plan treningowy
-
-
+              <Button
+                className="m-3"
+                onClick={() => (window.location.href = "/training_plan")}
+                size="lg"
+                id="submit"
+                type="button"
+                variant="success"
+              >
+                See your plan
+              </Button>
             </div>
           )}
         </div>
 
-        <div class="col-2">
-        </div>
-
+        <div class="col-2"></div>
       </div>
-
     </div>
   );
 };
 
 export default Quiz;
-
-
